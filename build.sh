19309b75c388496d5e3f88cf020de0ad58b948eb
mypepewallet <mypepewallet@gmail.com>
2025-01-12 00:26:31 +0800

    update version and dependencies

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-next build && next export && mv out/_next out/next && sed -i -e 's=/_next/=/next/=g' out/**.html && mv out/*.html build && yarn build-scripts &&  rsync -va --delete-after scripts/compiled/ build/scripts/ && rm -rf scripts/compiled && rsync -va --delete-after out/next/ build/next/ && rm -rf out && rsync -va public/ build/
+rm -rf .next
+rm -rf .cache
+mkdir -p build
+mkdir -p scripts/compiled
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  google-chrome http://reload.extensions
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  open -a "Google Chrome" http://reload.extensions
-fi
+next build && next export && \
+mv out/_next out/next && \
+sed -i -e 's=/_next/=/next/=g' out/**.html && \
+mv out/*.html build && \
+yarn build-scripts && \
+rsync -va --delete-after scripts/compiled/ build/scripts/ && \
+rm -rf scripts/compiled && \
+rsync -va --delete-after out/next/ build/next/ && \
+rm -rf out && \
+rsync -va public/ build/
